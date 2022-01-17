e102676947a0f69bbadfd556d92a2f2b0cbceb6c
rarestara <97609188+rarestara@users.noreply.github.com>
2022-01-17 13:06:19 +0200

    Improve EvenNumbers to be faster

--- src/com/java/algorithms/EvenNumbers.java
+++ src/com/java/algorithms/EvenNumbers.java
@@ -2,8 +2,8 @@ package com.java.algorithms;
 
 public class EvenNumbers {
     public static void main(String[] args) {
-        for(int i=0;i<=100;i++){
-            if(i%2==0) System.out.println(i);
+        for(int i=0;i<=100;i=i+2){
+            System.out.println(i);
         }
     }
 }
