3ae24950523fd1930134fefd6c1d59a697f70a6c
rarestara <97609188+rarestara@users.noreply.github.com>
2022-01-21 09:53:24 +0200

    Corrected errors

--- src/com/java/algorithms/Calculator.java
+++ src/com/java/algorithms/Calculator.java
@@ -3,43 +3,48 @@ package com.java.algorithms;
 import java.util.*;
 
 public class Calculator {
+
+    private static double Sum (long a,long b){
+        return a + b;
+    }
+    private static double Difference ( long a,long b){
+        return a - b;
+    }
+    private static double Product (long a,long b){
+        return a * b;
+    }
+    private static double Divide (long a,long b){
+        return a / b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Calculator");
-        Scanner scan = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
         System.out.print("Please enter first number: ");
-        int a = scan.nextInt();
+        long a = keyboard.nextLong();
         System.out.print("Please enter second number: ");
-        int b = scan.nextInt();
-        System.out.print("Please enter opperator O: ");
-        char 'O' = scan.nextInt();
+        long b = keyboard.nextLong();
+        keyboard.nextLine();
+        System.out.print("Please enter opperator: ");
 
-        private double Sum ( int a, int b){
-            return a + b;
+        char c = keyboard.next().charAt(0);
+        if (c == '+') {
+            System.out.print("Sum is: " + Sum(a,b));
         }
-        private double Difference (int a, int b){
-            return a - b;
+
+        else if (c == '-' ){
+            System.out.print("Difference is: " + Difference(a,b));
         }
-        private double Product ( int a, int b){
-            return a * b;
+
+        else if (c == '*' ){
+            System.out.print("Product is: " + Product(a,b));
         }
-        private double Divide ( int a, int b){
-            return a / b;
-        }
-        if char ('O' == '+') {
-            System.out.print("Sum is: " + Sum);
-        }
-        else if (char 'O' == '-' ){
-            System.out.print("Difference is: " + Difference);
-        }
-        else if (char 'O' == '*' ){
-            System.out.print("Product is: " + Product);
-        }
-        else if (char 'O' == '/' ){
-            System.out.print("Divide is: " + Divide);
+        else if (c == '/' ){
+            System.out.print("Divide is: " + Divide(a,b));
         }
         else{
             System.out.println("Wrong opperation.Please enter one of the valid opperations: + or - or * or /");
-            return;
+
         }
     }
 }
---
+++ src/com/java/algorithms/Powers.java
@@ -0,0 +1,19 @@
+package com.java.algorithms;
+
+import java.util.Scanner;
+
+public class Powers {
+    public static void main(String[] args) {
+        System.out.println("Powers");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter first number: ");
+        int a = scan.nextInt();
+        System.out.print("Please enter second number: ");
+        int b = scan.nextInt();
+        //System.out.print("a empowered by b is: " + powerFunction);
+
+    }
+    public long powerFunction ( int a, int b){
+            return 1;
+    }
+}
