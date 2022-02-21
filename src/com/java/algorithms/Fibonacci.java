769b15b3abd9c11bce3e867342b6f26a0df1597e
rarestara <97609188+rarestara@users.noreply.github.com>
2022-02-21 17:09:10 +0200

    Finished Fibonacci Sequence with dynamic programming plus recursive function

--- src/com/java/algorithms/Fibonacci.java
+++ src/com/java/algorithms/Fibonacci.java
@@ -9,7 +9,7 @@ public class Fibonacci {
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Please enter n number: ");
         int n = keyboard.nextInt();
-        int[] fibonacciSequence= new int[n];
+        /* int[] fibonacciSequence= new int[n];
         fibonacciSequence[0] = 0;
         fibonacciSequence[1]= 1;
         for(int i=2;i<n;i++) {
@@ -17,11 +17,21 @@ public class Fibonacci {
 
         }
         System.out.println("Fibonacci sequence for this number is "+ Arrays.toString(fibonacciSequence));
-
+*/
+        //Alternative solution with recursive function//
+        System.out.println("Fibonacci sequence for this number is "+ fib(n));
         }
+    public static int fib (int num){
+        if (num == 0){
+            return 0;
+        }
+        if (num==1){
+            return 1;
+        }
+        return fib(num-1)+ fib(num-2);
+    }
 
 
-// 0 1 1 2 3 5 8 13 21 34 55 89
     }
 
 
